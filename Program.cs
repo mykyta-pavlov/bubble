95ef1d6e66955b3b71c05013c4ce4f81cff5e93a
mykyta-pavlov <43242475+mykyta-pavlov@users.noreply.github.com>
2019-11-30 13:16:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace bubble
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = {6, 5, 3, 10, 8};
+            bool swapped;
+
+            foreach(int i in arr)
+                Console.Write($"{i} ");
+            
+            do
+            {
+                swapped = false;
+                for(int i = 0; i < arr.Length - 1; i++)
+                {
+                    if(arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }                    
+                }
+
+            } while(swapped == true);
+
+            Console.WriteLine();
+
+            for(int i = 0; i < arr.Length; i++)            
+                Console.Write($"{arr[i]} ");            
+        }
+    }
+}
